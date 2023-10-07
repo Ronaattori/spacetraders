506854960156f9e0aa8fcec4fd7251aa7cec7e2a
Roni Björn <roni.bjorn@gmail.com>
2023-10-07 17:13:16 +0300

    Create a basic side navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<aside class="flex flex-col fixed h-full w-24 bg-slate-300 shadow-md">
+    <slot/>
+</aside>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,9 @@
     import type { Agent } from '$lib/api-sdk';
     import { notifications } from '$lib/stores';
     import Notifications from '$lib/components/Notifications.svelte';
+    import Navbar from "$lib/components/Navbar.svelte";
+    import { IconListDetails, IconLogin, IconLogout, IconRegistered, IconSpace } from "@tabler/icons-svelte";
+    import Navbutton from "$lib/components/Navbutton.svelte";
     
     let myAgent: Agent;
     let location;
@@ -23,76 +26,26 @@
 
 <Notifications notifications={notifications}/>
 
-<div class="bg-yellow-700 page h-32 w-32">
-  <aside class="">
-    <button>
-      alöksjd
-    </button>
-  </aside>
-</div>
-
 <div class="page">
-  <!-- Sidebar -->
-  <aside class="navbar navbar-vertical navbar-expand-sm navbar-dark">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <h1 class="navbar-brand navbar-brand-autodark">
-        <a href="/">
-          <img src={img} width="110" height="32" alt="Tabler" class="navbar-brand-image">
-          Speis treiders
-        </a>
-      </h1>
-      <span>
-        Logged in as {username} 
-      </span>
-      <div class="collapse navbar-collapse" id="sidebar-menu">
-        <ul class="navbar-nav pt-lg-3">
-          <li class="nav-item">
-            <a class="nav-link" href="/">
-              <span class="nav-link-title">
-                Home
-              </span>
-            </a>
-          </li>
-          <li>
-          <div class="nav-item dropdown p-2 ">
-            <a class="btn dropdown-toggle" data-bs-toggle="dropdown">Contract</a>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="/contract">List</a>
-            </div>
-          </div>
-          <div class="nav-item dropdown p-2 ">
-            <a class="btn dropdown-toggle" data-bs-toggle="dropdown">User</a>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="/user/register">Register</a>
-              <a class="dropdown-item" href="/user/login">Login</a>
-              <button class="dropdown-item" on:click={() => localStorage.removeItem("apiKey")}>Logout</button>
-            </div>
-          </div>
-        </li>
-        </ul>
-        <ul class="card m-3 text-black">
-          <div class="card-body">
-            <h3 class="card-title">Location:</h3> 
-            <p class="text-secondary">Symbol: {myAgent?.headquarters}</p>
-            <p class="text-secondary">System: {myAgent?.headquarters.split("-").splice(0, 2).join("-")}</p>
-          </div>
-        </ul>
+  <Navbar>
+      <div>
+        <h1>Speis treiders</h1> 
+        <img src={img} class="p-2" alt="Munkki">
       </div>
-    </div>
-  </aside>
-  <div class="page-wrapper">
-    <header class="navbar navbar-light">
-      <div class="container-xl">
-        <div>
-          <span>Money: {curMoney}</span>
-          <span class="border mx-2"/>
-          <span>Galactic bitches: {0}</span>
-        </div>
-      </div>
-    </header>
+      <Navbutton href="/contract" icon={IconListDetails}>
+        <span>Contracts</span>
+      </Navbutton>
+      <Navbutton href="/user/login" icon={IconLogin}>
+        <span>Login</span>
+      </Navbutton>
+      <Navbutton href="/user/register" icon={IconRegistered}>
+        <span>Register</span>
+      </Navbutton>
+      <Navbutton href="/user/login" on:click={() => localStorage.removeItem("apiKey")} icon={IconLogout}>
+        <span>Logoff</span>
+      </Navbutton>
+  </Navbar>
+  <div class="pl-24">
     <slot/>
   </div>
 </div>
