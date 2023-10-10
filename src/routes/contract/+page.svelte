15f0407e2274127b2937777a96211be4cb9af659
Roni Björn <roni.bjorn@gmail.com>
2023-10-10 17:34:01 +0300

    Implement accepting contracts

--- src/routes/contract/+page.svelte
+++ src/routes/contract/+page.svelte
@@ -4,6 +4,7 @@
     import type { Contract } from '$lib/api-sdk';
     import ContractInfo from '$lib/components/ContractInfo.svelte';
     import Table from '$lib/components/Table.svelte';
+    import { notifications } from '$lib/stores';
 
     let contracts:Array<Contract> = [];
     onMount(async () => {
@@ -16,23 +17,27 @@
       if (!e) return;
       e.style.display == "none" ? e.style.display = "block" : e.style.display = "none";
     }
+    async function acceptContract(contractId:string) {
+      const contract = await $api.contracts.acceptContract(contractId);
+      notifications.success("Succesfully accepted contract");
+    }
 </script>
 
 <Table columns={["Info", "Type", "Faction", "Payment", ""]}>
   {#each contracts as contract}
     <tr>
-        <td>
-          <button class="btn btn-outline-secondary">
-            Info
-          </button>
-          {#if contract.terms.deliver}
-            <ContractInfo contract={contract}/>
-          {/if}
-        </td>
-        <td>{contract.type}</td>
-        <td>{contract.factionSymbol}</td>
-        <td>{contract.terms.payment.onAccepted} / {contract.terms.payment.onFulfilled}</td>
-        <td><a href="/contract/accept">Accept</a></td>
+      <td>
+        <button class="btn btn-outline-secondary">
+          Info
+        </button>
+        {#if contract.terms.deliver}
+          <ContractInfo contract={contract}/>
+        {/if}
+      </td>
+      <td>{contract.type}</td>
+      <td>{contract.factionSymbol}</td>
+      <td>{contract.terms.payment.onAccepted} / {contract.terms.payment.onFulfilled}</td>
+      <td><button class="btn" on:click={() => acceptContract(contract.id)}>Accept</button></td>
     </tr>       
   {/each}
 </Table>
