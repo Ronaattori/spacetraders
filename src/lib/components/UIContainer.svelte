fad77928feafe8ef7aeb9c0b338832120b658bb9
Roni Björn <roni.bjorn@gmail.com>
2023-11-11 16:13:25 +0200

    Add pointer events to UI elements

--- src/lib/components/UIContainer.svelte
+++ src/lib/components/UIContainer.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+    import { onMount } from "svelte";
 
+    let container: HTMLElement
+    
+    $: addPointerEvents(container);
+    function addPointerEvents(element: HTMLElement) {
+        if (!element) return; 
+        for (const child of element.children) {
+            if (!child.classList.contains("pointer-events-auto")) {
+                child.classList.add("pointer-events-auto");
+            }        
+        }
+    }
+        
 </script>
 
-<div class="absolute w-full h-full pointer-events-none">
+<div class="absolute w-full h-full pointer-events-none" bind:this={container}>
     <slot />
 </div>
